48fbd5e2bcfeebacf66332c7955707b38663e4bc
Kshitinjay <kshitinjay20@gmail.com>
2021-04-15 20:24:09 +0530

    final commit

--- zolve/src/components/Clipboard/index.js
+++ zolve/src/components/Clipboard/index.js
@@ -1,11 +1,30 @@
-import React from 'react'
-
+import React, { useState } from "react";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import "./style.css";
 const Clipboard = () => {
-    return (
+  const [inputValue, setInputValue] = useState("");
+  const [flag, setFlag] = useState(false);
+  const handleChange = (event) => {
+    setInputValue(event.target.value);
+  };
+  const copied = () => {
+    setFlag(true);
+    setTimeout(() => {
+      setFlag(false);
+    }, 2000);
+  };
+  return (
+    <div>
+      <h1>Clipboard</h1>
+      <input type="text" onChange={handleChange} value={inputValue} />
+      <CopyToClipboard text={inputValue} onCopy={copied}>
         <div>
-            <h1>Clipboard</h1>
+          <button>Copy</button>
+          {flag ? <p>Copied!</p> : null}
         </div>
-    )
-}
+      </CopyToClipboard>
+    </div>
+  );
+};
 
-export default Clipboard
+export default Clipboard;
